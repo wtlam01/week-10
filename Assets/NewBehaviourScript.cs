7bade29735f9131350ea95edfa73a7b1ec6c93d5
wtlam01 <wing8.lam@live.uwe.ac.uk>
2023-12-08 11:33:38 +0000

    1

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
+        Destroy(this.gameObject);
     }
 
     // Update is called once per frame
