cbf1180292cea10247d813a1934ed1c061f774eb
wtlam01 <wing8.lam@live.uwe.ac.uk>
2023-12-08 12:17:39 +0000

    the cube jump

--- Assets/collideCube.cs
+++ Assets/collideCube.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class collideCube : MonoBehaviour
 {
-    private Rigidbody erb
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
