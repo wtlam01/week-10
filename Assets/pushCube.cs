27ffb404c3dc7aacde6ab01e91a7c336f16ddd53
wtlam01 <wing8.lam@live.uwe.ac.uk>
2023-12-08 11:51:29 +0000

    give a counter

--- Assets/pushCube.cs
+++ Assets/pushCube.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class pushCube : MonoBehaviour
 {
+    private int Counter; // give a name  (Counter) or you can give a value
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World");
+        Counter = 0;
        // Destroy(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Counter++;
+        Debug.Log(Counter);
+        
         if(Input.GetKeyDown(KeyCode.Space))
         {
           Destroy(this.gameObject);   
